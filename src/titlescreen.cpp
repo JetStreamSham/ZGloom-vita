95fbd4c26c49c90ac7ad888dd786cbdd0beaf153
Jet <76616771+JetStreamSham@users.noreply.github.com>
2021-08-24 03:38:29 -0500

    fixed about screen exiting too quickly

--- src/titlescreen.cpp
+++ src/titlescreen.cpp
@@ -1,6 +1,6 @@
 #include "titlescreen.h"
 
-void TitleScreen::Render(SDL_Surface* src, SDL_Surface* dest, Font& font)
+void TitleScreen::Render(SDL_Surface *src, SDL_Surface *dest, Font &font)
 {
 	SDL_BlitSurface(src, nullptr, dest, nullptr);
 	bool flash = (timer / 5) & 1;
@@ -9,10 +9,14 @@ void TitleScreen::Render(SDL_Surface* src, SDL_Surface* dest, Font& font)
 
 	if (status == TITLESTATUS_MAIN)
 	{
-		if (flash || (selection != MAINENTRY_PLAY)) font.PrintMessage("PLAY", 150, dest, 1);
-		if (flash || (selection != MAINENTRY_SELECT)) font.PrintMessage("LEVEL SELECT", 170, dest, 1);
-		if (flash || (selection != MAINENTRY_ABOUT)) font.PrintMessage("ABOUT", 190, dest, 1);
-		if (flash || (selection != MAINENTRY_QUIT)) font.PrintMessage("EXIT", 210, dest, 1);
+		if (flash || (selection != MAINENTRY_PLAY))
+			font.PrintMessage("PLAY", 150, dest, 1);
+		if (flash || (selection != MAINENTRY_SELECT))
+			font.PrintMessage("LEVEL SELECT", 170, dest, 1);
+		if (flash || (selection != MAINENTRY_ABOUT))
+			font.PrintMessage("ABOUT", 190, dest, 1);
+		if (flash || (selection != MAINENTRY_QUIT))
+			font.PrintMessage("EXIT", 210, dest, 1);
 	}
 	else if (status == TITLESTATUS_SELECT)
 	{
@@ -20,7 +24,8 @@ void TitleScreen::Render(SDL_Surface* src, SDL_Surface* dest, Font& font)
 		{
 			if ((i >= 0) && (i < (int)levelnames.size()))
 			{
-				if (flash || (i != selection)) font.PrintMessage(levelnames[i], 100 + (i - selection) * 10, dest, 1);
+				if (flash || (i != selection))
+					font.PrintMessage(levelnames[i], 100 + (i - selection) * 10, dest, 1);
 			}
 		}
 	}
@@ -39,7 +44,7 @@ void TitleScreen::Render(SDL_Surface* src, SDL_Surface* dest, Font& font)
 		font.PrintMessage("UTILITIES CODED IN BLITZ BASIC 2", 130, dest, 1);
 		font.PrintMessage("RENDERED IN DPAINT3 AND DPAINT4", 140, dest, 1);
 		font.PrintMessage("DECRUNCHING CODE BY THOMAS SCHWARZ", 150, dest, 1);
-		
+
 		font.PrintMessage("PSVITA PORT BY JETSTREAMSHAM", 170, dest, 1); // added port
 	}
 }
@@ -51,42 +56,60 @@ TitleScreen::TitleScreen()
 	timer = 0;
 }
 
-TitleScreen::TitleReturn TitleScreen::Update(int& levelout)
+TitleScreen::TitleReturn TitleScreen::Update(int &levelout)
 {
 
 	if (status == TITLESTATUS_MAIN)
 	{
 
-		if (Input::GetButtonDown(SCE_CTRL_DOWN)) {
+		if (Input::GetButtonDown(SCE_CTRL_DOWN))
+		{
 			selection++;
-			if (selection == MAINENTRY_END) selection = MAINENTRY_END - 1;
+			if (selection == MAINENTRY_END)
+				selection = MAINENTRY_END - 1;
 		}
 
-		if (Input::GetButtonDown(SCE_CTRL_UP)) {
+		if (Input::GetButtonDown(SCE_CTRL_UP))
+		{
 			selection--;
-			if (selection == -1) selection = 0;
+			if (selection == -1)
+				selection = 0;
 		}
 
-		if (Input::GetButtonDown(SCE_CTRL_CROSS)) {
-			if (selection == MAINENTRY_PLAY) return TITLERET_PLAY;
-			if (selection == MAINENTRY_QUIT) return TITLERET_QUIT;
-			if (selection == MAINENTRY_ABOUT) status = TITLESTATUS_ABOUT;
-			if (selection == MAINENTRY_SELECT) { selection = 0; status = TITLESTATUS_SELECT; };
+		if (Input::GetButtonDown(SCE_CTRL_CROSS))
+		{
+			if (selection == MAINENTRY_PLAY)
+				return TITLERET_PLAY;
+			if (selection == MAINENTRY_QUIT)
+				return TITLERET_QUIT;
+			if (selection == MAINENTRY_ABOUT)
+				status = TITLESTATUS_ABOUT;
+			if (selection == MAINENTRY_SELECT)
+			{
+				selection = 0;
+				status = TITLESTATUS_SELECT;
+			};
 		}
-	} else 	if (status == TITLESTATUS_SELECT)
+	}
+	else if (status == TITLESTATUS_SELECT)
 	{
 
-		if (Input::GetButtonDown(SCE_CTRL_DOWN)) {
+		if (Input::GetButtonDown(SCE_CTRL_DOWN))
+		{
 			selection++;
-			if (selection == levelnames.size()) selection = levelnames.size() - 1;
+			if (selection == levelnames.size())
+				selection = levelnames.size() - 1;
 		}
 
-		if (Input::GetButtonDown(SCE_CTRL_UP)) {
+		if (Input::GetButtonDown(SCE_CTRL_UP))
+		{
 			selection--;
-			if (selection == -1) selection = 0;
+			if (selection == -1)
+				selection = 0;
 		}
 
-		if (Input::GetButtonDown(SCE_CTRL_CROSS)) {
+		if (Input::GetButtonDown(SCE_CTRL_CROSS))
+		{
 			levelout = selection;
 			status = TITLESTATUS_MAIN;
 			selection = 0;
@@ -95,9 +118,9 @@ TitleScreen::TitleReturn TitleScreen::Update(int& levelout)
 	}
 	else
 	{
-		status = TITLESTATUS_MAIN;
+		if (Input::GetButtonDown(SCE_CTRL_CROSS))
+			status = TITLESTATUS_MAIN;
 	}
-
 
 	return TITLERET_NOTHING;
 }
